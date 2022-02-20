aa4c18ab7d674e7dead80e9b2a8b3ad50f387cf3
Mariannadtd <romanova.lenovo@gmail.com>
2022-02-20 22:52:05 +0300

    preloader

---
+++ src/pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="isLoading">
+    loading
+  </div>
+  <div v-else>
+    content
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const isLoading = ref(true)
+
+  // setTimeout(() => {
+  //   isLoading.value = false
+  // }, 1000)
+
+    return { isLoading }
+  }
+}
+</script>
